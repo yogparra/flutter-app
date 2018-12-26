13ecf62e55d12db2b2c135cc63db70bd9d6236bf
Guillermo <gmo.parra@gmail.com>
2018-12-26 12:18:52 -0300

    Se agrega un steteful widget.            Se crear un LisView de scroll infinito.            Se agrega la carpeta screenshot al proyecto.

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # app_flutter
 
-A new Flutter project.
+flutter run -> ejecuta y traspasa al dispositivo de emulación
 
-## Getting Started
+flutter run -> modo hot reload 
+          r -> recarga los cambios
+          s -> screenshot del emulador en ejecución  
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+Commit 1.- Estructura inicial. 
+Commit 2.- Se agrega un steteful widget.
+           Se crear un LisView de scroll infinito.
+           Se agrega la carpeta screenshot al proyecto.
+           Se completa con mayor detelles al archivo README. 
--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,58 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+//StatelessWidget estado inmutable no puede cambiar
+class MyApp extends StatelessWidget {
   @override
-  Widget build (BuildContext context){
-
-    final wordPair = new WordPair.random();
-     
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'miApp',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('mi titulo'),
-          ),
-          body: Center(
-            child: Text(wordPair.asPascalCase),
-          ),
-      ),
-      );
+      home: RandonWords(),
+    );
   }
+}
+
+class RandonWordsState extends State<RandonWords> {
+  
+  final _lista = <WordPair>[];
+  final fuenteGrande = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Título listas'),
+      ),
+      body: _buildSuggestion(),
+    ); 
+  }
+
+  Widget _buildSuggestion(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i){
+        if (i.isOdd) return Divider();
+        final index = i ~/ 2;
+        if (index >= _lista.length){
+            _lista.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_lista[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: fuenteGrande,
+      ),
+    );
+  }
+}
+
+//StatefulWidget mantiene el estado que puede cambiar durante el tiempo de vida del widget
+class RandonWords extends StatefulWidget {
+  @override
+  RandonWordsState createState() => new RandonWordsState();
 }
